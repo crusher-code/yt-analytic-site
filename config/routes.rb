2258caf189442079411a92f3c3e837017eeada97
Joshua Gibson <joshalexgibs@gmail.com>
2019-08-05 18:59:33 +0000

    set up root page and devise config

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get '*path', to: 'pages#root', constraints: ->(request){ request.format.html? } #redefine
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index" #redefine homepage once created
 end
