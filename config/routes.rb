5291ce3d0947c2beae5c7ac443e009795aabc982
Dimitri <dimitricollas@gmail.com>
2019-08-06 20:47:41 +0000

    Changed the order of channels resources route in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :channels
   devise_for :users
   get '*path', to: 'pages#root', constraints: ->(request){ request.format.html? } #redefine
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :channels
   root to: "home#index" #redefine homepage once created
 end
