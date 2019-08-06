22d6079a5a6d30d5a76f6752d24ab0929dc84498
Jack Maldonado <maldonado_jack@yahoo.com>
2019-08-06 21:52:17 +0000

    testing setup

--- app/javascript/components/pages/__tests__/MainApp.js
+++ app/javascript/components/pages/__tests__/MainApp.js
@@ -1,12 +1,12 @@
   import React from 'react';
   import ReactDOM from 'react-dom';
-  import MainApp from './mainapp';
+  import MainApp from '../../MainApp';
   import Enzyme, { mount } from 'enzyme';
   import Adapter from 'enzyme-adapter-react-16';
 
   Enzyme.configure({ adapter: new Adapter() });
 
  it('Renders a LEARN welcome', ()=>{
-   const MainApp = mount(<MainApp />)
-  expect(MainApp.find('h2').text()).toEqual('Hello')
+   const mainapp = mount(<MainApp />)
+  expect(mainapp.find('h2').text()).toEqual('Hello')
  })
