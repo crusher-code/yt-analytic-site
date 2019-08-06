76ea8a1693cede240b1785141993d666b0b857e6
Ubuntu <ubuntu@ip-172-31-35-241.us-east-2.compute.internal>
2019-08-06 18:29:42 +0000

    Added channel table and assosiation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :channels
   devise_for :users
   get '*path', to: 'pages#root', constraints: ->(request){ request.format.html? } #redefine
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
