47171e6fcb1a32eea5fbad1450afa4a89ddac8b1
Jack Maldonado <maldonado_jack@yahoo.com>
2019-08-07 23:31:16 +0000

    Home page and other pages setup.

--- app/javascript/components/pages/AboutUs.js
+++ app/javascript/components/pages/AboutUs.js
@@ -4,7 +4,7 @@ import ReactDOM  from 'react-dom'
 class AboutUs extends Component{
     render(){
         return(
-            <h1>Homepage</h1>)
+            <h1>About Us</h1>)
     }
 }
 export default AboutUs
