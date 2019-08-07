55dfeb8f13aa29f460c705dab548485dc95c7d11
Jack Maldonado <maldonado_jack@yahoo.com>
2019-08-07 22:46:29 +0000

    username login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,11 @@
 class ApplicationController < ActionController::Base
-    before_action: configure_permitted, if: :devise_controler?
+    before_action :configure_permitted, if: :devise_controller?
     
     protected 
     def configure_permitted_parameters 
-        devise_parameter_santizer.permit(:sign_up, key [:username])
-        devise_parameter_saniizer.permt :sigh_up, key :add_attrs
-        devise_parameter_saniizer.permit :account_update, key :add_attrs
-        
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+        devise_parameter_sanitizer.permit :sign_up, keys: :add_attrs
+        devise_parameter_sanitizer.permit :account_update, keys: :add_attrs
         
     end 
     
