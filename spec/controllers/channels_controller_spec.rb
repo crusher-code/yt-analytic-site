d06e7df4cf8d110f0bc702e18d517e597cf056ec
Dimitri <dimitricollas@gmail.com>
2019-08-07 17:38:47 +0000

    Added devise to rspec

--- spec/controllers/channels_controller_spec.rb
+++ spec/controllers/channels_controller_spec.rb
@@ -1,1 +1,15 @@
 require 'rails_helper'
+RSpec.describe ChannelsController, :type => :controller do
+    describe "get index" do
+        it "will put a 200 status code" do
+            get :index
+            expect(response.status).to eq(200)
+        end
+    end
+    describe "get show" do
+        it "will put a 200 status code" do
+            get :show
+            expect(response.status).to eq(200)
+        end
+    end
+end
