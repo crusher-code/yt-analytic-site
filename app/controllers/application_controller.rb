80bd44f8c21652db222bb8e3c5bd66ec3d4f9233
Jack Maldonado <maldonado_jack@yahoo.com>
2019-08-07 21:51:50 +0000

    setting up Username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+    before_action: configure_permitted, if: :devise_controler?
+    
+    protected 
+    def configure_permitted_parameters 
+        devise_parameter_santizer.permit(:sign_up, key [:username])
+        devise_parameter_saniizer.permt :sigh_up, key :add_attrs
+        devise_parameter_saniizer.permit :account_update, key :add_attrs
+        
+        
+    end 
+    
+#   protect_from_forgery with: :exception
 end
