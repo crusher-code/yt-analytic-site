7915c6da746667aeb34d7bdccc178905401162f2
lali <lali20021@gmail.com>
2019-08-08 00:08:06 +0000

    application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,11 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted, if: :devise_controller?
+    before_action :configure_permitted_parameters, if: :devise_controller?
     
-    protected 
     def configure_permitted_parameters 
         devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-        devise_parameter_sanitizer.permit :sign_up, keys: :add_attrs
-        devise_parameter_sanitizer.permit :account_update, keys: :add_attrs
-        
     end 
     
 #   protect_from_forgery with: :exception
 end
+   
+ 
