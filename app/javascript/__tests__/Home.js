62c90d7a6dac20abd515fd38b57a8897688ccc97
Jack Maldonado <maldonado_jack@yahoo.com>
2019-08-14 22:01:56 +0000

    created test files

---
+++ app/javascript/__tests__/Home.js
@@ -0,0 +1,9 @@
+  import React from 'react';
+  import ReactDOM from 'react-dom';
+  import Home from '../components/Home';
+  import Enzyme, { shallow, mount } from 'enzyme';
+  import Adapter from 'enzyme-adapter-react-16';
+  import { MemoryRouter } from 'react-router';
+  import { Route } from 'react-router-dom';
+  
+  Enzyme.configure({ adapter: new Adapter() });
---
+++ app/javascript/__tests__/NewChannel.js
@@ -0,0 +1,9 @@
+  import React from 'react';
+  import ReactDOM from 'react-dom';
+  import NewChannel from '../components/NewChannel';
+  import Enzyme, { shallow, mount } from 'enzyme';
+  import Adapter from 'enzyme-adapter-react-16';
+  import { MemoryRouter } from 'react-router';
+  import { Route } from 'react-router-dom';
+  
+  Enzyme.configure({ adapter: new Adapter() });
