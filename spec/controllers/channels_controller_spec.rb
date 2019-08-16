62e4e23f36e38fb019b5b9cef9cd78ec8148e8d4
Joshua Gibson <joshalexgibs@gmail.com>
2019-08-16 16:26:58 +0000

    added tests

--- spec/controllers/channels_controller_spec.rb
+++ spec/controllers/channels_controller_spec.rb
@@ -1,15 +1,23 @@
 require 'rails_helper'
 RSpec.describe ChannelsController, :type => :controller do
+    let(:user){create(:user)}
     describe "get index" do
         it "will put a 200 status code" do
             get :index
             expect(response.status).to eq(200)
         end
     end
-    # describe "get show" do
-    #     it "will put a 200 status code" do
-    #         get :show
-    #         expect(response.status).to eq(200)
-    #     end
-    # end
+    describe "get show" do
+        it "will put a 200 status code" do
+            get :index
+            get :show, { id: 1 }
+            expect(response.status).to eq(200)
+        end
+    end
+    describe "post create" do
+        it "will return 400 status code" do
+            post :create
+            expect(response.status).to eq(400)
+        end
+    end
 end
