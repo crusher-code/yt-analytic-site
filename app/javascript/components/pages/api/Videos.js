bf0e516df63c2d3a41beb6e8ab85a0d564763333
Dimitri <dimitricollas@gmail.com>
2019-08-19 21:19:54 +0000

    Made custom like bar from divs

--- app/javascript/components/pages/api/Videos.js
+++ app/javascript/components/pages/api/Videos.js
@@ -9,13 +9,18 @@ class Videos extends Component {
   }
   addLikes=(a,b)=>{
     let added = Number(a) + Number(b)
-    return String(added)
+    return added
+  }
+  likePercent=(a,b)=>{
+    let total = this.addLikes(a,b)
+    return (`${String((Number(a)/total)*100)}%`)
   }
 
 // MAKE A VIDEO CLICKABLE RAP THE DIV IN THE MAP RETURN IN <a> TAG WITH A HREF OF href={`https://www.youtube.com/watch?v=${video.contentDetails.videoId}`}
 // EMBED CODE <iframe width="256" height="144" src={`https://www.youtube.com/embed/${video.contentDetails.videoId}`} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 // REFER DIMITRI TO PULL MEDIUM SIZE THUMBNAIL
-
+// <meter value={video.statistics.likeCount} min="0" max={this.addLikes(video.statistics.likeCount,video.statistics.dislikeCount)}></meter>
+//<progress value={video.statistics.likeCount} max={this.addLikes(video.statistics.likeCount,video.statistics.dislikeCount)}></progress>
 render(){
 const { videoData } = this.props
     return (
@@ -23,8 +28,10 @@ const { videoData } = this.props
     {videoData &&
         <CardGroup>
             {this.props.videoData.map((video, index) => {
+            let widthLike = this.likePercent(video.statistics.likeCount,video.statistics.dislikeCount)
+            let styleLikeBar = {height:"5px",width:widthLike, backgroundColor:"#0388fc",borderRadius:"10px"}
                 return(
-                  <Col xs="3" style={{paddingBottom:"10px", paddingTop:"10px"}}>
+                  <Col key={index} xs="3" style={{paddingBottom:"10px", paddingTop:"10px"}}>
                     <Card key={index} style={{height:"100%"}}>
                       <CardBody>
                         <a target="_blank" rel="noopener noreferrer" href={`https://www.youtube.com/watch?v=${video.id}`}>
@@ -34,7 +41,9 @@ const { videoData } = this.props
                         <CardSubtitle>Views: {video.statistics.viewCount}</CardSubtitle>
                         <CardSubtitle>Likes: {`${video.statistics.likeCount}`}</CardSubtitle>
                         <CardSubtitle>Dislikes: {`${video.statistics.dislikeCount}`}</CardSubtitle>
-                        <meter value={video.statistics.likeCount} min="0" max={this.addLikes(video.statistics.likeCount,video.statistics.dislikeCount)}></meter>
+                        <div style={{height:"5px",width:"100%" , backgroundColor:"#858585", borderRadius:"10px"}}>
+                          <div style={styleLikeBar}></div>
+                        </div>
                       </CardBody>
                     </Card>
                   </Col>
