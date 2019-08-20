4799162921a8f2536ab1072b65effed0b801441c
learn <learn@learn04.local>
2019-08-20 11:40:47 -0700

    changed Home.js; styled button, rearranged text

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,27 +1,34 @@
 import React, {Component} from 'react'
 import ReactDOM  from 'react-dom'
 import mainpic from '../images/mainpic'
+import { Button } from 'reactstrap'
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
 class Home extends Component{
-    
-    render(){
-        const { channels, deleteChannel, logged_in } = this.props
-        return(
+  render(){
+    const { channels, deleteChannel, logged_in } = this.props
+      return(
+        <div>
+          {/*move this to css such that it can be overlayed*/}
+          <img style={{width: '100%', height: '100%'}} src={mainpic} alt=""/>
+          {logged_in &&
             <div>
-            <img  style={{width: '105%', height: '105%'}}src={mainpic} alt=""/>
-              {logged_in &&
-               <div>
-               {channels &&
+              {channels &&
                 <div>
                 {channels.map((channel, index) => {
-                    return(<p key={index}><button onClick={() => deleteChannel(channel.id)}>DELETE</button><Link to={`/analytics/${channel.id}`}>{channel.name}, {channel.id_channel}, {channel.id}</Link></p>)
+                  return(
+                    <p key={index}>
+                      <Link to={`/analytics/${channel.id}`} style={{fontSize:"16px"}}>{channel.id}: {channel.name}     </Link>
+                      <Button outline color="danger" size="sm" style={{height:"25px",paddingTop:"2px"}} onClick={() => deleteChannel(channel.id)}>X</Button>
+                      <br></br>
+                    </p>
+                  )
                 })}
                 </div>
-               }
-               </div>
               }
             </div>
+          }
+        </div>
         )
     }
 }
